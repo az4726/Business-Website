02c1f58f0d29cf0586e6a62a7ea79a448487eedb
Azhar Zaman <azhar.zaman99@gmail.com>
2018-08-23 10:35:22 +0100

    Completed development on this website

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var sliderIndex = 1;
+showSlides(sliderIndex);
+
+function plusSlides(n){
+  showSlides(sliderIndex += n)
+}
+
+function showSlides(n){
+  let slider = document.getElementsByClassName("slider");
+
+  if(n > slider.length){
+    sliderIndex = 1
+  }
+  if(n < 1){
+    sliderIndex = slider.length
+  }
+
+  for(i = 0; i < slider.length; i++){
+    slider[i].style.display = "none";
+  }
+
+  slider[sliderIndex - 1].style.display = "block";
+}
